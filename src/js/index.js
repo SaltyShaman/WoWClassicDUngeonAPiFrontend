d99f0336ce5bf440b1953af4ccb30a06211e7e54
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2025-04-11 13:13:38 +0200

    Integration fixes

--- src/js/index.js
+++ src/js/index.js
@@ -181,3 +181,122 @@ async function askAboutCharacter(event) {
     }
 }
 
+//Realms:
+const realms = [
+    { name: 'Aerie Peak', slug: 'aerie-peak' },
+    { name: 'Aggramar', slug: 'aggramar' },
+    { name: 'Al\'Akir', slug: 'al-akir' },
+    { name: 'Alonsus', slug: 'alonsus' },
+    { name: 'Anachronos', slug: 'anachronos' },
+    { name: 'Anub\'arak', slug: 'anubarak' },
+    { name: 'Argent Dawn', slug: 'argent-dawn' },
+    { name: 'Balnazzar', slug: 'balnazzar' },
+    { name: 'Bloodfeather', slug: 'bloodfeather' },
+    { name: 'Blade\'s Edge', slug: 'blades-edge' },
+    { name: 'Bladefist', slug: 'bladefist' },
+    { name: 'Bronzebeard', slug: 'bronzebeard' },
+    { name: 'Burning Steppes', slug: 'burning-steppes' },
+    { name: 'Chromaggus', slug: 'chromaggus' },
+    { name: 'Crushridge', slug: 'crushridge' },
+    { name: 'Darkspear', slug: 'darkspear' },
+    { name: 'Darksorrow', slug: 'darksorrow' },
+    { name: 'Dentarg', slug: 'dentarg' },
+    { name: 'Dragonmaw', slug: 'dragonmaw' },
+    { name: 'Drak\'thul', slug: 'drakthul' },
+    { name: 'Draenor', slug: 'draenor' },
+    { name: 'Exodar', slug: 'exodar' },
+    { name: 'Genjuros', slug: 'genjuros' },
+    { name: 'Hakkar', slug: 'hakkar' },
+    { name: 'Hellscream', slug: 'hellscream' },
+    { name: 'Karazhan', slug: 'karazhan' },
+    { name: 'Kilrogg', slug: 'kilrogg' },
+    { name: 'Kor\'gall', slug: 'korgall' },
+    { name: 'Laughing Skull', slug: 'laughing-skull' },
+    { name: 'Lightbringer', slug: 'lightbringer' },
+    { name: 'Magtheridon', slug: 'magtheridon' },
+    { name: 'Malygos', slug: 'malygos' },
+    { name: 'Moonglade', slug: 'moonglade' },
+    { name: 'Nordrassil', slug: 'nordrassil' },
+    { name: 'Outland', slug: 'outland' },
+    { name: 'Quel\'Thalas', slug: 'quelthalas' },
+    { name: 'Ragnaros', slug: 'ragnaros' },
+    { name: 'Runetotem', slug: 'runetotem' },
+    { name: 'Scarshield Legion', slug: 'scarshield-legion' },
+    { name: 'Shattered Halls', slug: 'shattered-halls' },
+    { name: 'Shattered Hand', slug: 'shattered-hand' },
+    { name: 'Shadowsong', slug: 'shadowsong' },
+    { name: 'Silvermoon', slug: 'silvermoon' },
+    { name: 'Skullcrusher', slug: 'skullcrusher' },
+    { name: 'Spinebreaker', slug: 'spinebreaker' },
+    { name: 'Stormrage', slug: 'stormrage' },
+    { name: 'Stormreaver', slug: 'stormreaver' },
+    { name: 'Stormscale', slug: 'stormscale' },
+    { name: 'Sylvanas', slug: 'sylvanas' },
+    { name: 'Taren Mill', slug: 'tarren-mill' },
+    { name: 'Terenas', slug: 'terenas' },
+    { name: 'The Maelstrom', slug: 'the-maelstrom' },
+    { name: 'The Venture Co', slug: 'the-venture-co' },
+    { name: 'Thunderhorn', slug: 'thunderhorn' },
+    { name: 'Turalyon', slug: 'turalyon' },
+    { name: 'Twisting Nether', slug: 'twisting-nether' },
+    { name: 'Vashj', slug: 'vashj' },
+    { name: 'Vek\'nilash', slug: 'veknilash' },
+    { name: 'Wildhammer', slug: 'wildhammer' },
+    { name: 'Xavius', slug: 'xavius' },
+    { name: 'Zenedar', slug: 'zenedar' }
+];
+
+
+//Drop down realm
+async function populateRealmDropdown() {
+    const realmDropdown = document.getElementById('wow-realm-name');
+    realmDropdown.innerHTML = ''; // Clear existing options
+
+    const defaultOption = document.createElement('option');
+    defaultOption.value = '';
+    defaultOption.textContent = 'Select Realm';
+    realmDropdown.appendChild(defaultOption);
+
+    realms.forEach((realm) => {
+        const option = document.createElement('option');
+        option.value = realm.slug;
+        option.textContent = realm.name;
+        realmDropdown.appendChild(option);
+    });
+}
+
+function showSuggestions(value) {
+    const resultsContainer = document.getElementById('autocomplete-results');
+    resultsContainer.innerHTML = '';
+    const filteredRealms = realms.filter(realm => realm.name.toLowerCase().includes(value.toLowerCase()));
+
+    if (filteredRealms.length > 0) {
+        resultsContainer.style.display = 'block';
+        filteredRealms.forEach(realm => {
+            const div = document.createElement('div');
+            div.textContent = realm.name;
+            div.addEventListener('click', function () {
+                document.getElementById('wow-realm-name').value = realm.name;
+                resultsContainer.style.display = 'none';
+            });
+            resultsContainer.appendChild(div);
+        });
+    } else {
+        resultsContainer.style.display = 'none';
+    }
+}
+
+document.getElementById('wow-realm-name').addEventListener('input', function () {
+    const value = this.value;
+    if (value.length > 0) {
+        showSuggestions(value);
+    } else {
+        document.getElementById('autocomplete-results').style.display = 'none';
+    }
+});
+
+document.addEventListener('click', function (event) {
+    if (!event.target.closest('#wow-realm-name')) {
+        document.getElementById('autocomplete-results').style.display = 'none';
+    }
+});
